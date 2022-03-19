8d8304c84872b46d2a9710dae1fd2b082a084d70
akhmed <abulaysovv@mail.ru>
2022-03-19 16:59:53 +0300

    last in django

--- Django Developer/hyperjob/login/urls.py
+++ Django Developer/hyperjob/login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 from .views import *
 
+
 urlpatterns = [
     path('login', MyLoginView.as_view()),
     path('login/', RedirectView.as_view(url='/login')),
--- Django Developer/hyperjob/resume/urls.py
+++ Django Developer/hyperjob/resume/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 from .views import *
 
+
 urlpatterns = [
     path('resumes/', ResumePage.as_view()),
     path('resume/new', ResumePage.as_view()),
